b98d215f85063394b2831faf64ab0e9ea39f1b24
Ivo Mishev <ivomishev@gmail.com>
2025-05-10 21:49:47 +0300

    Added some entities

--- src/main/java/com/fibank/balance/BalanceRepository.java
+++ src/main/java/com/fibank/balance/BalanceRepository.java
@@ -1,9 +1,11 @@
 package com.fibank.balance;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface BalanceRepository extends JpaRepository<Balance, Long> {
 
+  @EntityGraph(attributePaths = "denominations")
   Optional<Balance> findByCashierNameAndCurrency(String cashierName, Currency currency);
 }
